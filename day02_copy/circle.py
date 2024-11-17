95a42fae5492994a8ee4260e3d0d3b1e26c8473c
Karin Vardy <karinv32@gmail.com>
2024-11-17 11:35:25 +0200

    Delete day02_copy directory

--- day02_copy/circle.py
+++
@@ -1,8 +0,0 @@
-import math
-radius=float(input('Enter radius: '))
-pi=math.pi
-area=pi*(radius**2)
-circumference=2*pi*radius
-
-print("The area of the circle is:",area)
-print("The circumference of the circle is:",circumference)
--- day02_copy/rectangle.py
+++
@@ -1,7 +0,0 @@
-width=float(input('Enter width: '))
-length=float(input('Enter length: '))
-area=width*length
-perimeter=2*(width+length)
-
-print("The area of the rectangle is:",area)
-print("The perimeter of the rectangle is:",perimeter)
