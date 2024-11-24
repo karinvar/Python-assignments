5b5ff9bb0cfe8a175bdc84eb3a3a2bb2ba550b20
karinvar <karinv32@gmail.com>
2024-11-24 11:58:19 +0200

    change

--- Day03/circle.py
+++ Day03/circle.py
@@ -8,18 +8,20 @@ def area(radius):
 # Set up the argument parser
 parser = argparse.ArgumentParser(description="Calculate the area of a circle.")
 parser.add_argument(
-    "--radius",
+    "--radius",  # The argument we'll use in the command line
     type=float,
     help="Radius of the circle"
 )
 
-# Parse the arguments
+# Parse the arguments passed from the command line
 args = parser.parse_args()
 
-# Get the radius from the command line or prompt interactively
+# If the radius was passed as an argument, use it; otherwise, ask for interactive input
 if args.radius is None:
     radius = float(input("Please enter the radius of the circle: "))
 else:
     radius = args.radius
 
+# Calculate and print the area
 print("The area of the circle is:", area(radius))
+
