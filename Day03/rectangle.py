c4cee333dc83f1e5f4efcd0e3114c7986c3c08bb
karinvar <karinv32@gmail.com>
2024-11-24 11:41:28 +0200

    change

--- Day03/rectangle.py
+++ Day03/rectangle.py
@@ -2,6 +2,5 @@ width=float(input('Enter width: '))
 length=float(input('Enter length: '))
 area=width*length
 perimeter=2*(width+length)
-
 print("The area of the rectangle is:",area)
 print("The perimeter of the rectangle is:",perimeter)
